c9b50f4a466beff428fe531bd13306fb7fbe5031
Ariyan Rehman Razon <ariyan@outlook.com>
2021-09-18 23:21:50 +0800

    cat generator added

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,7 @@
 //Age Convert in days
 
-function ageInDays () {
-    var birthYear =  prompt('What Year You Born?');
+function ageInDays() {
+    var birthYear = prompt('What Year You Born?');
     var ageInDayss = (2018 - birthYear) * 365;
     var h1 = document.createElement('h1');
     var textAnswer = document.createTextNode('You are ' + ageInDayss + ' days old');
@@ -10,8 +10,15 @@ function ageInDays () {
     document.getElementById('flex-box-result').appendChild(h1);
 }
 
-function reset () {
+function reset() {
     document.getElementById('ageInDays').remove();
 }
 
 
+//Cat Generator
+function generateCat() {
+    var image = document.createElement('img');
+    var div = document.getElementById('flex-cat-gen');
+    image.src = "static/images/cat.jpg"
+    div.appendChild(image);
+}
