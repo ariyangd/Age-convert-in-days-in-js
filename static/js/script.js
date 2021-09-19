202f11de5dc9ae30a9efe30722c9ab2833cd87ed
Ariyan Rehman Razon <ariyan@outlook.com>
2021-09-19 17:12:27 +0800

    added Rock Paper Scissor Game

--- static/js/script.js
+++ static/js/script.js
@@ -16,9 +16,110 @@ function reset() {
 
 
 //Cat Generator
+
 function generateCat() {
     var image = document.createElement('img');
     var div = document.getElementById('flex-cat-gen');
     image.src = "static/images/cat.jpg"
     div.appendChild(image);
 }
+
+
+//Rock Paper Scissors
+
+function rpsGame(yourChoice) {
+    console.log(yourChoice);
+
+    var humanChoice, botChoice;
+    humanChoice = yourChoice.id;
+
+    botChoice = numberToChoice(ranToRpsInt());
+    console.log('Computer Choice:', botChoice);
+
+    result = decideWinner(humanChoice, botChoice);
+    console.log(result);
+
+    message = finalMessage(result);
+    console.log(message);
+
+    rpsFrontEnd(yourChoice.id, botChoice, message);
+
+}
+
+function ranToRpsInt() {
+    return Math.floor(Math.random() * 3);
+}
+
+function numberToChoice(number) {
+    return ['rock', 'paper', 'scissors'][number];
+}
+
+function decideWinner(yourChoice, computerChoice) {
+    var rpsDatabase = {
+        'rock': {
+            'scissors': 1,
+            'rock': 0.5,
+            'paper': 0
+        },
+        'paper': {
+            'rock': 1,
+            'paper': 0.5,
+            'scissors': 0
+        },
+        'scissors': {
+            'paper': 1,
+            'scissors': 0.5,
+            'rock': 0
+        }
+    }
+    var yourScore = rpsDatabase[yourChoice][computerChoice];
+    var computerScore = rpsDatabase[computerChoice][yourChoice];
+
+    return [yourScore, computerScore];
+}
+
+function finalMessage([yourScore, computerScore]) {
+    if (yourScore === 0) {
+        return {
+            'message': 'you lost',
+            'color': 'red'
+        };
+    } else if (yourScore === 0.5) {
+        return {
+            'message': 'you tied',
+            'color': 'yellow'
+        };
+    } else {
+        return {
+            'message': 'you won',
+            'color': 'green'
+        };
+    }
+}
+
+function rpsFrontEnd(humanImageChoice, botImageChoice, finalMessage) {
+    var imagesDatabase = {
+        'rock': document.getElementById('rock').src,
+        'paper': document.getElementById('paper').src,
+        'scissors': document.getElementById('scissors').src
+    }
+
+    document.getElementById('rock').remove();
+    document.getElementById('paper').remove();
+    document.getElementById('scissors').remove();
+
+    var humanDiv = document.createElement('div');
+    var botDiv = document.createElement('div');
+    var messageDiv = document.createElement('div');
+
+    humanDiv.innerHTML = "<img src='" + imagesDatabase[humanImageChoice] + "'height = '150' width='150'>"
+    messageDiv.innerHTML = "<h1 style='color: " + finalMessage['color'] + "; font-size: 60px padding: 30px; '>" + finalMessage['message'] + "</h1>";
+    botDiv.innerHTML = "<img src='" + imagesDatabase[botImageChoice] + "'height = '150' width='150'>";
+
+    document.getElementById('flex-box-rps-div').appendChild(humanDiv);
+    document.getElementById('flex-box-rps-div').appendChild(messageDiv);
+    document.getElementById('flex-box-rps-div').appendChild(botDiv);
+
+}
+
+//Button Color Change
