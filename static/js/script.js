8c475b3bdce459678ea0aba5db8f6d5ef03ae627
Ariyan Rehman Razon <ariyan@outlook.com>
2021-09-19 21:05:33 +0800

    added button color change feature

--- static/js/script.js
+++ static/js/script.js
@@ -123,3 +123,55 @@ function rpsFrontEnd(humanImageChoice, botImageChoice, finalMessage) {
 }
 
 //Button Color Change
+
+var all_buttons = document.getElementsByTagName('button');
+
+var copyAllButtons = [];
+for (let i = 0; i < all_buttons.length; i++) {
+    copyAllButtons.push(all_buttons[i].classList[1]);
+}
+
+
+
+function buttonColorChange(buttonThingy) {
+    if (buttonThingy.value === 'red') {
+        buttonRed();
+    } else if (buttonThingy.value === 'green') {
+        buttonGreen();
+    } else if (buttonThingy.value === 'reset') {
+        buttonColorReset();
+    } else if (buttonThingy.value === 'random') {
+        randomColors();
+    }
+}
+
+function buttonRed() {
+    for (let i = 0; i < all_buttons.length; i++) {
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add('btn-danger');
+    }
+}
+
+function buttonGreen (){
+    for (let i = 0; i < all_buttons.length; i++) {
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add('btn-success');
+    }
+}
+
+function buttonColorReset (){
+    for (let i = 0; i < all_buttons.length; i++) {
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add(copyAllButtons[i]);
+    }
+}
+
+function randomColors () {
+    var choices = ['btn-primary', 'btn-danger', 'btn-success', 'btn-warning'];
+
+for (let i = 0; i < all_buttons.length; i++) {
+        var randomNumber = Math.floor(Math.random() * 4);
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add(choices[randomNumber]);
+}
+}
